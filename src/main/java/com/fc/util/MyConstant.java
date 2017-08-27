1ada1be4b774d5ce774d0f85fde1c94799d3d25d
yangyu1992 <1634232934@qq.com>
2017-08-27 19:23:29 +0800

    yangyu

---
+++ src/main/java/com/fc/util/MyConstant.java
@@ -0,0 +1,23 @@
+package com.fc.util;
+
+
+public class MyConstant {
+    //七牛云相关
+    public static final String QINIU_IMAGE_URL = "http://otbx7hhlk.bkt.clouddn.com/";
+    public static final String QINIU_ACCESS_KEY = "eBCl2R5Hj1YmEbJQ_js8wefqcT8bThATtURvoixD";
+    public static final String QINIU_SECRET_KEY = "qKJw93-HCHPXCQG95TgZld2ZBivAhOI-cUmfO0FL";
+    public static final String QINIU_BUCKET_NAME = "yang";
+
+    //发送邮件的邮箱，要与df.properties中的一致
+    public static final String MAIL_FROM = "fanchao100@yeah.net";
+
+    //域名
+    public static final String DOMAIN_NAME = "http://localhost:8080/df/";
+    //public static final String DOMAIN_NAME = "http://www.doublefuck.top/";
+
+    //三种操作
+    public static final int OPERATION_CLICK_LIKE = 1;
+    public static final int OPERATION_REPLY = 2;
+    public static final int OPERATION_COMMENT = 3;
+
+}
